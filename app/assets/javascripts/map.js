a821377c05a4e457738ec6e3d7cb14bbd895a13d
Robin Zuschke <robin.l.zuschke@gmail.com>
2019-05-15 17:06:07 +0200

    reverted to ES5

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,17 +1,17 @@
+"use strict";
+
 function initMap(coordinates) {
     var centerCoordinates = new google.maps.LatLng(coordinates.centerLat, coordinates.centerLng);
     var mapOptions = {
         center: centerCoordinates,
         zoom: 13
     };
-    
     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-    coordinates.markers.forEach(marker => {
-        const coords = new google.maps.LatLng(marker.latitude, marker.longitude);
+    coordinates.markers.forEach(function (marker) {
+        var coords = new google.maps.LatLng(marker.latitude, marker.longitude);
         new google.maps.Marker({
             position: coords,
             map: map
         });
-    });   
+    });
 }
