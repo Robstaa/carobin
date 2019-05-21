95971809a09a07ff626f53b97f47cfe5c32145c1
Robin Zuschke <robin.l.zuschke@gmail.com>
2019-05-22 01:03:02 +0200

    Guard geocoder

--- app/services/geocoder_service.rb
+++ app/services/geocoder_service.rb
@@ -1,10 +1,14 @@
 module GeocoderService
   def latitude(address)
+    return nil if address.blank?
+
     results = Geocoder.search(address)
     results.first.coordinates[0]
   end
 
   def longitude(address)
+    return nil if address.blank?
+
     results = Geocoder.search(address)
     results.first.coordinates[1]
   end
