82f2c3db044c2cf1686066b1b3a23ef23abec02a
Robin Zuschke <robin.l.zuschke@gmail.com>
2019-05-22 01:03:29 +0200

    Edit place validations

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,8 @@
 class Place < ApplicationRecord
+  before_save :nil_if_blank
+
   has_many :visits
   accepts_nested_attributes_for :visits
-  validates :name, :latitude, :longitude, :location, presence: true
+  validates :name, :location, presence: true
+  validates :location, uniqueness: true
 end
