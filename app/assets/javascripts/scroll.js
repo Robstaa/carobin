8e3b7fdc9f51ecb296d7efb0917d702c6d511549
Robin Zuschke <robin.l.zuschke@gmail.com>
2019-05-23 08:15:07 +0200

    Design Homepage

---
+++ app/assets/javascripts/scroll.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function ($) {
+
+  $(".scroll-link").click(function (event) {
+    event.preventDefault();
+
+    var defaultAnchorOffset = 0;
+
+    var $anchor = $('#' + this.hash.substring(1));
+
+    var anchorOffset = $anchor.data('anchor-offset');
+    if (!anchorOffset) // for when anchor doesn't have the offset attribute like Section 4
+      anchorOffset = defaultAnchorOffset;
+
+    $('html,body').animate({
+      scrollTop: $anchor.offset().top - anchorOffset
+    }, 500);
+  })
+});
